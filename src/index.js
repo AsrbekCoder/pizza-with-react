f4553a419cd4685642f7747ac085ad2661afdea1
Asrbek <87761511+AsrbekCoder@users.noreply.github.com>
2021-12-06 21:27:00 +0500

    router and pages done

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./scss/app.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
